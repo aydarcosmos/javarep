18ccd13764f2e44ae41212386004f24905277c2e
Aidar <gumerov1993@mail.ru>
2018-12-04 12:52:36 +0300

    third commit for gitjavatest

--- gittest.java
+++ gittest.java
@@ -1,6 +1,7 @@
 class gittest{
   public static void main(String args[]){
-    System.out.print("Hello Git"); 
-    System.out.print("Hello Git");
+    System.out.print("Hello Git1"); 
+    System.out.print("Hello Git2");
+    System.out.print("Hello Git3");
   }
 }
